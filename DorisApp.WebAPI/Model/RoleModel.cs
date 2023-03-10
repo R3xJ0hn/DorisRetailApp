dacb4beb9c5efd3cf55b28764037460bcad8ff5e
RJ <pogirawsirex@gmail.com>
2023-03-10 17:40:46 +0800

    Add Role and User Models in WebApi

---
+++ DorisApp.WebAPI/Model/RoleModel.cs
@@ -0,0 +1,15 @@
+﻿namespace DorisApp.WebAPI.Model
+{
+    public class RoleModel
+    {
+        public int Id { get; set; }
+        public string RoleName { get; set; }
+        public int CreatedByUserId { get; set; }
+        public int UpdatedByUserId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+    }
+
+
+}
---
+++ DorisApp.WebAPI/Model/UserModel.cs
@@ -0,0 +1,19 @@
+﻿namespace DorisApp.WebAPI.Model
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public int RoleId { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string EmailAddress { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+        public string LastPasswordHash { get; set; } = string.Empty;
+        public DateTime LastPasswordCanged { get; set; }
+        public string Token { get; set; } = string.Empty;
+        public DateTime TokenCreated { get; set; }
+        public DateTime TokenExpires { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
