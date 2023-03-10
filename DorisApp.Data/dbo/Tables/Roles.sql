a2907453ceed7ee5afa84d3892860d8de5c8cf52
RJ <pogirawsirex@gmail.com>
2023-03-10 17:36:36 +0800

    Add Database Roles and Users Table

---
+++ DorisApp.Data/dbo/Tables/Roles.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Roles]
+(
+	[Id]			    INT NOT NULL PRIMARY KEY IDENTITY, 
+    [RoleName]			NVARCHAR(100) NOT NULL,
+	[CreatedByUserId]	INT DEFAULT 0 NOT NULL,
+	[UpdatedByUserId]	INT DEFAULT 0 NOT NULL,
+	[CreatedAt]			DATETIME2 NOT NULL DEFAULT getutcdate(), 
+	[UpdatedAt]			DATETIME2 NOT NULL DEFAULT getutcdate(), 
+)
---
+++ DorisApp.Data/dbo/Tables/Users.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[Users]
+(
+	[Id]                 INT NOT NULL PRIMARY KEY IDENTITY, 
+	[RoleID]			 INT NOT NULL,
+	[FirstName]			 NVARCHAR(50) NOT NULL, 
+	[LastName]			 NVARCHAR(50) NOT NULL,
+	[EmailAddress]		 NVARCHAR(256) NOT NULL UNIQUE, 
+	[PasswordHash]		 NVARCHAR(MAX) NOT NULL, 
+	[LastPasswordHash]	 NVARCHAR(MAX) NOT NULL, 
+	[LastPasswordCanged] DATETIME2 NOT NULL,
+	[Token]				 NVARCHAR(MAX) NOT NULL,
+    [TokenCreated]		 DATETIME2 NOT NULL, 
+    [TokenExpires]		 DATETIME2 NOT NULL, 
+	[CreatedAt]			 DATETIME2 NOT NULL DEFAULT getutcdate(), 
+	[UpdatedAt]			 DATETIME2 NOT NULL DEFAULT getutcdate(), 
+)
