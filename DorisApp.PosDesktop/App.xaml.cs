827f07f21cf2a4f5825ef9a7d929d3448965a1c3
RJ <pogirawsirex@gmail.com>
2023-04-12 11:22:56 +0800

    Add the WPF project for POS desktop
    Add wpf project and set up the dependency injection

---
+++ DorisApp.PosDesktop/App.xaml.cs
@@ -0,0 +1,40 @@
+﻿using DorisApp.PosDesktop;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Windows;
+
+namespace Pos
+{
+    public partial class App : Application
+    {
+        public IHost? AppHost { get; set; }
+
+        public App()
+        {
+            AppHost = Host.CreateDefaultBuilder()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddSingleton<MainWindow>();
+
+                }).Build();
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            await AppHost!.StartAsync();
+
+            var startupForm = AppHost.Services.GetRequiredService<MainWindow>();
+            startupForm.Show();
+
+            base.OnStartup(e);
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            await AppHost!.StopAsync();
+            base.OnExit(e);
+        }
+
+
+    }
+}
